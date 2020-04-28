4a4e8691d3db701de09cf92cf91b0e5f9be5474c
Pradeep Kumar Sahoo <pradeep17sep@gmail.com>
2020-04-28 10:02:00 +0530

    added memento

---
+++ src/design_patterns/behavioral_patterns/p5_mediator/Command.java
@@ -0,0 +1,6 @@
+package design_patterns.behavioral_patterns.p5_mediator;
+
+// colleague
+public interface Command {
+    void execute();
+}
---
+++ src/design_patterns/behavioral_patterns/p6_memento/EmployeeMemento.java
@@ -0,0 +1,22 @@
+package design_patterns.behavioral_patterns.p6_memento;
+
+// memento
+// Note: we have not added address, means we don't want to save address field.
+// Note: In case of Serialization, we can mark a field transient, to get the same result
+public class EmployeeMemento {
+    String name;
+    String phone;
+
+    public EmployeeMemento(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+}
