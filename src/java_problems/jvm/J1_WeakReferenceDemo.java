80d71a1e99beff49d806b0953d8ad462d0e6fb2f
Pradeep Sahoo <sahoo.pradeep.nits@gmail.com>
2020-06-26 11:12:09 +0530

    Added Reference Class Demo

---
+++ src/java_problems/jvm/J1_WeakReferenceDemo.java
@@ -0,0 +1,24 @@
+package java_problems.jvm;
+
+import java.lang.ref.WeakReference;
+
+public class J1_WeakReferenceDemo {
+    public static void main(String[] args) {
+        Person person = new Person();
+        WeakReference<Person> wr = new WeakReference<>(person);
+        Person p1 = wr.get();
+        System.out.println(p1);
+
+        person = null;
+        p1 = null;
+        Person p2 = wr.get();
+        System.out.println(p2);
+
+        p2 = null;
+        System.gc();
+        Person p3 = wr.get();
+        System.out.println(p3);
+    }
+
+    private static class Person {}
+}
