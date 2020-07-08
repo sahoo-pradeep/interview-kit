f6717e21cf26a94ced8da9e97248ce8967e7159d
Pradeep Sahoo <sahoo.pradeep.nits@gmail.com>
2020-07-08 11:20:44 +0530

    Small change in Builder Pattern

---
+++ src/test/Test.java
@@ -0,0 +1,10 @@
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.text.Normalizer;
+import java.util.*;
+
+public class Test {
+    public static void main(String[] args) {
+        EnumSet.noneOf(Normalizer.Form.class);
+    }
+}
